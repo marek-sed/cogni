486f810421093c94267f0a408c1fb353e7533b30
marek <skl@kistler.com>
2016-02-25 14:47:01 +0100

    Revert "fix createSession"
    This reverts commit faa05cc358212f3dfe3fde9263704e6ba0fbd3ce.

--- shared/reducers/sessionInfoReducer.js
+++ shared/reducers/sessionInfoReducer.js
@@ -1,11 +1,6 @@
-import {UPDATE_SETUP_SUCCESS} from '../actions/actionTypes';
 
-function reducer(state = window.__SESSION_INFO__, action) {
+function reducer(state = window.__SESSION_INFO__) {
   console.log('session info reducer ', state)
-  switch (action.type) {
-    case UPDATE_SETUP_SUCCESS:
-      return state.setIn('isActive', true);
-  }
   return state;
 }
 
