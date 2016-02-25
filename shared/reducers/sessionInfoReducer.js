faa05cc358212f3dfe3fde9263704e6ba0fbd3ce
marek <skl@kistler.com>
2016-02-25 14:33:14 +0100

    fix createSession

--- shared/reducers/sessionInfoReducer.js
+++ shared/reducers/sessionInfoReducer.js
@@ -1,6 +1,11 @@
+import {UPDATE_SETUP_SUCCESS} from '../actions/actionTypes';
 
-function reducer(state = window.__SESSION_INFO__) {
+function reducer(state = window.__SESSION_INFO__, action) {
   console.log('session info reducer ', state)
+  switch (action.type) {
+    case UPDATE_SETUP_SUCCESS:
+      return state.setIn('isActive', true);
+  }
   return state;
 }
 
